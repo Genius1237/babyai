84d39da98baa71ad85260c17e91f57b9c2940dfe
Genius1237 <anirudhsriniv>
2019-04-22 12:01:01 -0400

    Implement methods of grouping stages in curriculum together

--- babyai/rl/algos/rcppo.py
+++ babyai/rl/algos/rcppo.py
@@ -16,8 +16,9 @@ import traceback
 import sys
 import os
 import itertools
+import math
 
-def generator(env_name, demo_loc):
+def generator(env_name, demo_loc, curr_method):
     demos = babyai.utils.demos.load_demos(demo_loc)
 
     seed = 0
@@ -29,8 +30,12 @@ def generator(env_name, demo_loc):
         env.reset()
         envs.append(env)
 
+    states = []
+    curr_done = False
     for ll in range(max_len):
-        states = []
+        if ll == max_len - 1:
+            curr_done=True
+
         for i,demo in enumerate(demos):
             actions = demo[3]
             
@@ -45,21 +50,35 @@ def generator(env_name, demo_loc):
             env.count=0
             states.append(env)
         
-        yield states
+        if curr_method == 'one':
+            yield states,curr_done
+            states = []
+        elif curr_method == 'four':
+            if ll%4 == 3 or curr_done:
+                yield states,curr_done
+                states = []
+        elif curr_method == 'log':
+            if math.log2(ll+2) == int(math.log2(ll+2)) or curr_done:
+                yield states,curr_done
 
-def worker(conn, random_seed, env_name, demo_loc):
+def worker(conn, random_seed, env_name, demo_loc, curr_method):
     #babyai.utils.seed(0)
     random.seed(random_seed)
     
-    start_state_generator = generator(env_name, demo_loc)
+    start_state_generator = generator(env_name, demo_loc, curr_method)
 
     i=0
     #good_start_states = []
-    for good_start_states in start_state_generator:
+    for good_start_states,curr_done in start_state_generator:
         #good_start_states.extend(good_start_states_u)
         if i==0:
             i+=1
             env = copy.deepcopy(random.choice(good_start_states))
+        else:
+            if curr_done:
+                conn.send("curr_done")
+            else:
+                conn.send("done")
         while True:
             try:
                 cmd, data = conn.recv()
@@ -77,7 +96,6 @@ def worker(conn, random_seed, env_name, demo_loc):
                     #print(env,env.mission)
                     conn.send(env.count)
                 elif cmd == "update":
-                    conn.send("done")
                     break
                 else:
                     raise NotImplementedError
@@ -88,7 +106,7 @@ def worker(conn, random_seed, env_name, demo_loc):
 class RCParallelEnv(gym.Env):
     """A concurrent execution of environments in multiple processes."""
     
-    def __init__(self, env_name, n_env, demo_loc):
+    def __init__(self, env_name, n_env, demo_loc, curr_method):
         assert n_env >= 1, "No environment given."
 
         self.env_name = env_name
@@ -103,7 +121,7 @@ class RCParallelEnv(gym.Env):
         for i in range(self.n_env):
             local, remote = Pipe()
             self.locals.append(local)
-            p = Process(target=worker, args=(remote, rand_seed+i, env_name, demo_loc))
+            p = Process(target=worker, args=(remote, rand_seed+i, env_name, demo_loc,curr_method))
             p.daemon = True
             p.start()
             remote.close()
@@ -140,14 +158,18 @@ class RCParallelEnv(gym.Env):
     def update_good_start_states(self):
         #print(sys.getrefcount(good_start_states),sys.getsizeof(good_start_states))
         [local.send(("update",None)) for local in self.locals]
-        [local.recv() for local in self.locals]
+        t = [local.recv() for local in self.locals]
+        if t[0] == "curr_done":
+            return True
+        else:
+            return False
 
 class RCPPOAlgo(PPOAlgo):
     """
     The class containing an application of Reverse Curriculum learning from
     https://arxiv.org/pdf/1707.05300.pdf to Proximal Policy Optimization
     """
-    def __init__(self, env_name, n_env, acmodel, demo_loc, version, es_method=2, update_frequency = 10, transfer_ratio=0.15, random_walk_length=1, num_frames_per_proc=None, discount=0.99, lr=7e-4, beta1=0.9, beta2=0.999,
+    def __init__(self, env_name, n_env, acmodel, demo_loc, version, es_method=2, update_frequency = 10, transfer_ratio=0.15, random_walk_length=1, curr_method = 'one',num_frames_per_proc=None, discount=0.99, lr=7e-4, beta1=0.9, beta2=0.999,
                  gae_lambda=0.95,
                  entropy_coef=0.01, value_loss_coef=0.5, max_grad_norm=0.5, recurrence=4,
                  adam_eps=1e-5, clip_eps=0.2, epochs=4, batch_size=256, preprocess_obss=None,
@@ -169,7 +191,7 @@ class RCPPOAlgo(PPOAlgo):
         elif version == "v2" or version == "v3":
             self.read_good_start_states_v2(env_name,demo_loc)
         self.env = None
-        self.env = RCParallelEnv(self.env_name,self.n_env, demo_loc)
+        self.env = RCParallelEnv(self.env_name,self.n_env, demo_loc, curr_method)
         self.obs = self.env.reset()
         
         self.update = 0
@@ -177,6 +199,8 @@ class RCPPOAlgo(PPOAlgo):
         self.log_history = []
         self.es_max = -1
         self.es_pat = 0
+        self.curr_done = False
+        self.curr_really_done = False
 
     def early_stopping_check(self, method, bound):
         '''
@@ -255,29 +279,34 @@ class RCPPOAlgo(PPOAlgo):
                 self.env = ParallelEnv([gym.make(self.env_name) for _ in range(self.n_env)])
         
         elif self.version == "v3":
-            if self.update % self.update_frequency == 0:
+            if self.update % self.update_frequency == 0 and not self.curr_really_done:
                 success_rate = np.mean([1 if r > 0 else 0 for r in logs['return_per_episode']])
                 self.log_history.append(success_rate)
                 logger = logging.getLogger(__name__)
 
                 min_delta = 0.025
                 patience = 1
-                if self.curr_update < self.curriculum_length:
+                if self.es_method == 1:
+                    bound = 0.95
+                elif self.es_method == 2:
+                    bound = 0.7+(self.curr_update/self.curriculum_length)*(0.99-0.7)
+
+                if not self.curr_done:
                     #if self.early_stopping_check(patience+(self.curr_update),min_delta):
-                    if self.es_method == 1:
-                        bound = 0.95
-                    elif self.es_method ==2:
-                        bound = 0.7+(self.curr_update/self.curriculum_length)*(0.99-0.7)
                     if self.early_stopping_check(self.es_method,bound):    
                         self.curr_update+=1
                         self.log_history = []
-                        self.env.update_good_start_states()
-                        logger.info('Start state Update Number {}/{}'.format(self.curr_update,self.curriculum_length))
+                        self.curr_done = self.env.update_good_start_states()
+                        logger.info('Start state Update Number {}'.format(self.curr_update))
                 
-                elif self.curr_update == self.curriculum_length:
-                    self.curr_update += 1
-                    logger.info('Start State Updates Done')
-                    self.env = ParallelEnv([gym.make(self.env_name) for _ in range(self.n_env)])
+                else:
+                    if self.early_stopping_check(self.es_method,bound):
+                        self.curr_update += 1
+                        self.log_history = []
+                        logger.info('Start State Updates Done')
+                        
+                        self.env = ParallelEnv([gym.make(self.env_name) for _ in range(self.n_env)])
+                        self.curr_really_done = True
 
 
         #self.obs = self.env.reset()
